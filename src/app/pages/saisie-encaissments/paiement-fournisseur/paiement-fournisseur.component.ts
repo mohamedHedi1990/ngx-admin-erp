31abe4c649046df5ece4aaef438c8e42e3f75363
Hedi Boussada <hboussada-ext@ALMERYS.local>
2020-09-29 13:57:38 +0100

    Saisie treserorie : paiements Fournisseurs

--- src/app/pages/saisie-encaissments/paiement-fournisseur/paiement-fournisseur.component.ts
+++ src/app/pages/saisie-encaissments/paiement-fournisseur/paiement-fournisseur.component.ts
@@ -10,43 +10,31 @@ import {ConfirmationService} from 'primeng/api';
 })
 export class PaiementFournisseurComponent implements OnInit {
 
-  showProviderInvoiceWindow = false;
   invoices = [];
   selectedInvoices = [];
   loading = false;
-  invoice = null;
   constructor(private UtilsService: UtilsServiceService,
               public dialogService: DialogService, private confirmationService: ConfirmationService) { }
 
   ngOnInit(): void {
-    this.initInvoice();
     this.getAllInvoices();
   }
-  saveInvoice(invoice) {
+  savePaymentRule(invoice) {
 
     const context = this;
     this.UtilsService.post(UtilsServiceService.API_PROVIDER_INVOICE, invoice).subscribe( response => {
-        this.hideInvoiceWindow();
-        if ( invoice.invoiceId == null) {
-          this.UtilsService.showToast('success',
-            'Facture ajoutée avec succés',
-            `La facture fournisseur numéro  ${invoice.invoiceNumber} a été ajoutée avec succcés`);
-        } else {
-          this.UtilsService.showToast('success',
-            'Facture modfiée avec succés',
-            `La facture fournisseur numéro  ${invoice.invoiceNumber} a été modifiée avec succcés`);
-        }
+
+      this.UtilsService.showToast('success',
+      'Réglement ajoutée avec succés',
+      `Un réglement a été ajoutée avec succés à la facture ${invoice.invoiceNumber}`);
         context.getAllInvoices();
-        context.initInvoice();
       },
       error => {this.UtilsService.showToast('danger',
         'Erreur interne',
-        `Un erreur interne a été produit lors de la souvegar de facture fournisseur numéro  ${invoice.invoiceNumber}`); });
+        `Un erreur interne a été produit lors de l'ajout du réglement`); });
 
   }
-  hideInvoiceWindow() {
-    this.showProviderInvoiceWindow = false;
-  }
+
 
   getAllInvoices() {
     const context = this;
@@ -60,53 +48,6 @@ export class PaiementFournisseurComponent implements OnInit {
       });
   }
 
-  editInvoice(invoice) {
-    this.invoice = invoice;
-    this.showProviderInvoiceWindow = true;
-  }
 
-  delInvoice(invoice) {
-    const context = this;
-    const url = UtilsServiceService.API_INVOICE + '/' + invoice.invoiceId;
-    this.UtilsService.delete(`${UtilsServiceService.API_PROVIDER_INVOICE}/${invoice.invoiceId}`).subscribe( response => {
-        this.UtilsService.showToast('success',
-          'Facture supprimée avec succés',
-          `La facture fournisseur numéro  ${invoice.invoiceNumber} a été supprimée avec succcés`);
-        context.getAllInvoices();
-      },
-      error => {this.UtilsService.showToast('danger',
-        'Erreur interne',
-        `Un erreur interne a été produit lors de la suppression de facture fournisseur numéro  ${invoice.invoiceNumber}`);
-      });
-
-
-  }
-
-  deleteInvoice(invoice) {
-    this.confirmationService.confirm({
-      message: `Voulez vous vraiment supprimer la facture fournisseur numéro  ${invoice.invoiceNumber}?`,
-      acceptLabel: 'Supprimer',
-      rejectLabel: 'Annuler',
-      header: `Supprimer une facture`,
-      acceptButtonStyleClass: 'p-button-danger',
-      rejectButtonStyleClass: 'p-button-secondary',
-      accept: () => {
-        this.delInvoice(invoice);
-      },
-    });
-  }
-  initInvoice() {
-    this.invoice = {
-      invoiceId: null,
-      invoiceNumber: '',
-      provider: null,
-      invoiceDate: null,
-      invoiceDeadlineDate: null,
-      invoiceNet: 0,
-      invoiceRs: 0,
-      invoiceRsType: 'VALUE',
-      invoiceTotalAmount: 0,
-    };
-  }
 
 }
