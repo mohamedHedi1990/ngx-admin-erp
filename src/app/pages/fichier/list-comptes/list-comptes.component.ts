7a2059ec2c6cedfe95ffc41e5c9f049ddf405b5d
Hedi Boussada <hboussada-ext@ALMERYS.local>
2020-11-06 17:37:48 +0100

    modif

--- src/app/pages/fichier/list-comptes/list-comptes.component.ts
+++ src/app/pages/fichier/list-comptes/list-comptes.component.ts
@@ -131,7 +131,7 @@ export class ListComptesComponent implements OnInit {
         accountChargeCustomerPhoneNumber: '',
         accountNumber: '',
         accountRIB: '',
-        accountCurrency: '',
+        accountCurrency: 'TND',
         accountContacts: [],
         accountInitialAmount: null,
     };
