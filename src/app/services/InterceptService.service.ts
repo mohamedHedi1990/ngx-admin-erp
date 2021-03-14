c98551313914523c18f04cd0ae4855fc0c18e889
LamiaMessaoudi <34386091+LamiaMessaoudi@users.noreply.github.com>
2021-03-14 19:57:33 +0100

    redirection to login page if token expired or not authorized (#58)

--- src/app/services/InterceptService.service.ts
+++ src/app/services/InterceptService.service.ts
@@ -10,12 +10,14 @@ import {
 // RxJS
 import { Observable } from "rxjs";
 import { tap } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 /**
  * More information there => https://medium.com/@MetonymyQT/angular-http-interceptors-what-are-they-and-how-to-use-them-52e060321088
  */
 @Injectable()
 export class InterceptService implements HttpInterceptor {
+  constructor(private router: Router){}
   // intercept request and add token
   intercept(
     request: HttpRequest<any>,
@@ -44,6 +46,10 @@ console.log('tttttt');
         },
         (error) => {
           // http response status code
+          if (error.status === 401||error.status === 403){
+            localStorage.clear();
+            this.router.navigateByUrl('/auth/login');
+          }
           console.log("----response----");
           console.error(error.status);
           console.error(error.message);
